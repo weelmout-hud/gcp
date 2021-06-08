3d3ad7eb3c2446fdd52d321c3a5bc6867df3012b
wellMout <barnaber13@gmail.com>
2021-06-08 05:32:44 +0200

    Gcloud storge scripts

--- gcf1/Storage_lines/list_blobs.py
+++ gcf1/Storage_lines/list_blobs.py
@@ -5,4 +5,5 @@ os.environ.setdefault("GCLOUD_PROJECT", "even-kite-315518")
 client = storage.Client()
 bucket = storage.Bucket(client, "hcl_storage", user_project="even-kite-315518")
 all_blobs = list(client.list_blobs(bucket))
+
 print(all_blobs)
